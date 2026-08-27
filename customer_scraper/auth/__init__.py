f94a2683395d3cfd2b39d73f00ec821ee3a6e00c
Adarsh Kauthale <adrshkauthale2032@gmail.com>
2026-08-27 15:21:54 +0530

    Changes

--- customer_scraper/auth/__init__.py
+++ customer_scraper/auth/__init__.py
@@ -5,8 +5,7 @@ Authentication package for customer scraper.
 from .auth_manager import AuthManager, AuthExpiredError
 from .playwright_session import PlaywrightSessionHandler
 from .chrome_session import (
-    resolve_chrome_paths,
-    extract_cookies_from_chrome_db,
+    parse_curl_command,
     extract_full_session_from_chrome,
 )
 
@@ -14,7 +13,7 @@ __all__ = [
     "AuthManager",
     "AuthExpiredError",
     "PlaywrightSessionHandler",
-    "resolve_chrome_paths",
-    "extract_cookies_from_chrome_db",
+    "parse_curl_command",
     "extract_full_session_from_chrome",
 ]
+
